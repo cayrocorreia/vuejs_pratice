4a6934fe529916f0a75c6e62537e28e699c0221d
C1313334 Cayro Correia Oliveira <c1313334@interno.bb.com.br>
2020-08-21 22:33:15 -0300

    praticando vuejs

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container fluid>
+        <div class="pills">
+            <Pill v-for="word in groupWords" :key="word.name" 
+            :name="word.name" :amount="word.amount"  />
+
+        </div>
+    </v-container>  
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+
+    data: function () {
+        return {
+            groupWords: [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 400 },
+                { name: 'he', amount: 555 },
+            ]
+        }
+
+    }
+ 
+}
+</script>
+
+<style>
+.pills{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+</style>
